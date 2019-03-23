20867fc8877cb82a32c080e51f8555c98313b220
Anten Skrabec <cebarks@gmail.com>
2019-03-23 16:25:00 -0600

    reworks seed data and tables for many-many relationship

--- seeds/favorites.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function(knex, Promise) {
-  return knex('favorites').del()
-    .then(function () {
-      return Promise.all([
-        knex('favorites').insert({"name": "We Will Rock You", "artist_name": "Queen", "genre": "Rock", "rating": 88 }),
-        knex('favorites').insert({"name": "Bohemian Rhapsody", "artist_name": "Queen", "genre": "Rock", "rating": 66 })
-      ]);
-    });
-};
