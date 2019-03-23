f1e676c5251cdc94adb98e7483ab70c5c9ca78fc
Anten Skrabec <cebarks@gmail.com>
2019-03-23 12:33:50 -0600

    adds testing for favorites show endpoint

--- test/favorites.test.js
+++ test/favorites.test.js
@@ -49,29 +49,23 @@ describe("Favorites Endpoints", () => {
 
       done();
     });
+  });
 
-    it("GET /api/v1/favorites", (done) => {
-      chai.request(app)
-      .get('/api/v1/favorites')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
+  it("GET /api/v1/favorites/:id", (done) => {
+    chai.request(app)
+    .get('/api/v1/favorites/1')
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
 
-        expect(res.body[0].id).to.eq(1);
-        expect(res.body[0].name).to.eq("We Will Rock You");
-        expect(res.body[0].artist_name).to.eq("Queen");
-        expect(res.body[0].genre).to.eq("Rock");
-        expect(res.body[0].rating).to.eq(88);
+      expect(res.body[0].id).to.eq(1);
+      expect(res.body[0].name).to.eq("We Will Rock You");
+      expect(res.body[0].artist_name).to.eq("Queen");
+      expect(res.body[0].genre).to.eq("Rock");
+      expect(res.body[0].rating).to.eq(88);
 
-        expect(res.body[1].id).to.eq(2);
-        expect(res.body[1].name).to.eq("Bohemian Rhapsody");
-        expect(res.body[1].artist_name).to.eq("Queen");
-        expect(res.body[1].genre).to.eq("Rock");
-        expect(res.body[1].rating).to.eq(66);
-
-        done();
-      });
+      done();
     });
   });
 });
