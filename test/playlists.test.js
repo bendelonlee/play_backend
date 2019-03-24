3ac1ebfde98c8c1e14a3b6bbecce633a4597e792
Anten Skrabec <cebarks@gmail.com>
2019-03-23 19:20:31 -0600

    adds initial test and implementation for playlist index endpoint

--- test/playlists.test.js
+++ test/playlists.test.js
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 const app = require('../index').app;
 const db = require('knex')(require('../knexfile')['development']);
 
-describe("Favorites Endpoints", () => {
+describe("Playlists Endpoints", () => {
   before((done) => {
     db.raw("TRUNCATE playlists_favorites restart identity;")
     .then(() => db.raw("TRUNCATE playlists restart identity CASCADE;"))
@@ -25,5 +25,25 @@ describe("Favorites Endpoints", () => {
     db.seed.run()
     .then(() => done())
     .catch(error => { throw error; });
+  });
+
+  it("GET /api/v1/playlists", (done) => {
+    chai.request(app)
+    .get('/api/v1/playlists')
+    .end((err, res) => {
+      console.log(res.body)
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+
+      expect(res.body[0].id).to.eq(1);
+      expect(res.body[0].title).to.eq("playlist1");
+      expect(res.body[0].favorites[0]).to.eq({});
+
+      expect(res.body[1].id).to.eq(2);
+      expect(res.body[1].title).to.eq("playlist2");
+
+      done();
+    });
   });
 });
