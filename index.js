e3fbca0a0b726c60ed9b3f6b32a2c45e9c10e4a8
Anten Skrabec <cebarks@gmail.com>
2019-03-25 16:01:54 -0600

    MVC refactor of playlist favorite create endpoint

--- index.js
+++ index.js
@@ -16,34 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Play Backend';
 
-function handleError(error, response) {
-  console.log(error)
-  response.status(500).json({ error });
-}
-
 app.use('/api/v1/favorites', favoritesRoutes);
-
 app.use('/api/v1/playlists', playlistsRoutes);
-
-const favorite = require('./lib/models/favorite');
-const playlist = require('./lib/models/playlist');
-
-app.post('/api/v1/playlists/:playlist_id/favorites/:favorite_id', (request, response) => {
-  database('playlists_favorites')
-  .innerJoin('favorites', 'favorites.id', 'playlists_favorites.favorite_id')
-  .innerJoin('playlists', 'playlists.id', 'playlists_favorites.playlist_id')
-  .insert({"favorite_id": request.params.favorite_id, "playlist_id": request.params.playlist_id}, ['playlist_id', 'favorite_id'])
-  .then(playlists_favorite => {
-    return Promise.all([
-      favorite.find(request.params.favorite_id),
-      playlist.find(request.params.playlist_id)
-    ]);
-  })
-  .then(objs => {
-    response.status(201).json({"message": `Successfully added ${objs[0][0].name} to ${objs[1][0].title}`})
-  })
-  .catch(error => handleError(error, response));
-});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
