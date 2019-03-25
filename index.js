8f0decf76bb806c7cf54138b52ebccb95da3d517
Anten Skrabec <cebarks@gmail.com>
2019-03-25 12:15:55 -0600

    moves rest of favorites endpoints to MVC style

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Play Backend';
 
-const favorite_allowed_props = ['name', 'artist_name', 'genre', 'rating'];
-
 function handleError(error, response) {
   console.log(error)
   response.status(500).json({ error });
@@ -27,60 +25,9 @@ function handleError(error, response) {
 
 app.use('/api/v1/favorites', favoritesRoutes);
 
-app.get('/api/v1/favorites/:id', (request, response) => {
-  Favorite.find(request.params.id)
-    .then((favorite) => {
-      response.status(200).json(favorite);
-    })
-    .catch((error) => handleError(error, response));
-});
+// app.put('/api/v1/favorites/:id', );
 
-app.post('/api/v1/favorites', (request, response) => {
-  const favorite = request.body;
-
-  var missing = [];
-
-  for (let requiredParameter of favorite_allowed_props) {
-    if (!favorite_allowed_props.includes(requiredParameter)) {
-      missing.push(requiredParameter);
-    }
-  }
-
-  if (missing.length > 0) {
-    return response.status(422)
-      .send({ error: `Expected format: { name: <String>, artist_name: <String>, genre: <String>, rating: <Integer> }. You're missing "${missing.join(', ')}" properties.` });
-  }
-
-  Favorite.create(favorite)
-    .then(favorite => {
-      response.status(201).json(favorite[0]);
-    })
-    .catch((error) => handleError(error, response));
-});
-
-app.put('/api/v1/favorites/:id', (request, response) => {
-  const favorite = request.body
-
-  for(var prop of Object.keys(favorite)) {
-    if(!favorite_allowed_props.includes(prop)) {
-      response.status(400).json({"error": `"${prop}" is not a valid, updatable property for "favorite".`})
-    }
-  }
-
-  Favorite.update(request.params.id, favorite)
-    .then(favorite => {
-      response.status(200).json(favorite[0]);
-    })
-    .catch((error) => handleError(error, response));
-});
-
-app.delete('/api/v1/favorites/:id', (request, response) => {
-  Favorite.del(request.params.id)
-    .then(() => {
-      response.status(204).send();
-    })
-    .catch((error) => handleError(error, response));
-});
+// app.delete('/api/v1/favorites/:id', );
 
 app.get('/api/v1/playlists', (request, response) => {
   database('playlists')
